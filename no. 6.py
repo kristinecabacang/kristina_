429918eb7e699cd9370f8dcea0f61bbecc7a1c03
kristinecabacang <167757770+kristinecabacang@users.noreply.github.com>
2024-04-22 17:51:16 +0800

    Update no. 6.py

--- no. 6.py
+++ no. 6.py
@@ -1,14 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Generate x values
 x = np.linspace(-10, 10, 1000)
 x = x[x !=0]
 
-# Calculate y values using the equation 
 y = np.sin(x) / (3*x)
 
-# Create the plot
 plt.figure(figsize=(8, 6))
 plt.plot(x, y, label='$y =\\frac{{\\sin(x)}}{{3x}}$', color='g', linewidth=2)
 plt.axhline(y=0, color='y')
