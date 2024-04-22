7400b2c05f4aebdc7d7d472757f020dd01de4fc9
kristinecabacang <167757770+kristinecabacang@users.noreply.github.com>
2024-04-22 17:51:56 +0800

    Update no. 8.py

--- no. 8.py
+++ no. 8.py
@@ -1,14 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Generate x values
 x = np.linspace(-10, 10, 1000)
 x = x[x !=0]
 
-# Calculate y values using the equation 
 y = np.cos(x) / (5*x)
 
-# Create the plot
 plt.figure(figsize=(8, 6))
 plt.plot(x, y, label='$y =\\frac{{\\cos(x)}}{{5x}}$', color='b', linewidth=2)
 plt.axhline(y=0, color='y')
